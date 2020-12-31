d864842e362c64e2b9d3e107ef9cc9f3ba51bd4f
jelmer <jelmer@teamcreative.nl>
2020-12-31 13:47:16 +0100

    Basic plugin setup

---
+++ src/Lynx.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace mynamespace;
+
+class Lynx extends \craft\base\Plugin
+{
+    public function init()
+    {
+        parent::init();
+
+        // Custom initialization code goes here...
+    }
+}
